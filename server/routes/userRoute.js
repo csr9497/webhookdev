681be13cf70d8598eb487db54799e3caa42ada2f
Cesar Oliva <coliva@inticousa.com>
2020-01-07 19:14:40 -0500

    fixes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,26 +1,61 @@
 const express= require('express')
 const app= express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const opti = (req) =>{
+
+const corsOptions = {
+    origin: ['192.168.100.16'],
+    // methods: 'GET'
+}
+app.use(morgan('dev'))
+app.use(cors(corsOptions))
+//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.raw())
+
+const opti = (req, payload) =>{
     const rp = require('request-promise')
-    headers= { 'x-api-key': 'af6fdaaa-b9a5-4d26-9844-5c0a6b4cb2db' , 'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMTQzYWU1OTNmNTE5MDk5MGZiNjlhNiIsImlhdCI6MTU3ODM4NDEwMSwiZXhwIjoxNTc4NDcwNTAxfQ.gA3RaGniesUevBJixf4GEuunOhTCQPqIKginNhPDxFk"}
-    const body = req.body.content
-    const data = { data:body, type: 'provider'}
-    const dir = `http://192.168.1.39:8000/service/5e143ae593f5190990fb69a6`
+    headers = {
+        'x-api-key': 'af6fdaaa-b9a5-4d26-9844-5c0a6b4cb2db', 
+        'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMTRkOWI3M2YyYWRiNGFhZjJlZjE4NiIsImlhdCI6MTU3ODQyNDc1OSwiZXhwIjoxNTc4NTExMTU5fQ.iZ4iBErkXKCWbJXp_JvqAaxP0OhETNcaGEvYWJ638lI'}
+    const {body } = req
+    //console.log(body)
+    const dir = `http://192.168.100.16:8000/service/5e14d9b73f2adb4aaf2ef186`
     const consult = async () => {
     var options = {
-    method:'POST',
-    url: dir,
-    headers: headers,
-    json: true,
-    form: data
-}
-return result = await rp(options)
+        method:'POST',
+        url: dir,
+        headers: headers,
+        json: true,
+        form: payload
+        }
+    return result = await rp(options)
     }
     return consult
 }
-app.post('/chargebee/payment2',(req,res) => {
-    res.send('Data recivied')
-    setTimeout( opti(req),3000)
+app.post('/chargebee/payment',(req,res) => {
+    const payload = req.headers.card == req.headers.key ? toSend(req, 'ok') : toSend(req,'fail')
+    res.send(req.body)
+    setTimeout( opti(req, payload),3000)
 }) 
 module.exports = app
+
+
+const toSend = ({ body}, status) =>{
+
+    const responseToSend = { 
+        response: {
+            service: 'chargeebee',
+            date:'now',
+            number: Date.now() /10*10,
+            operation: body.data.operation,
+            status: status,
+            detail: `${status} operation`,
+            data: body.data
+        }
+    }
+    return responseToSend   
+}
--- server/server.js
+++ server/server.js
@@ -2,12 +2,9 @@ const express= require('express')
 const app= express()
 const bodyParser= require('body-parser')
 const mongoose= require('mongoose')
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/x-www-form-urlencode
 // parse application/json
-app.use(bodyParser.json())
 app.use( require('./routes/userRoute'))
-
 app.listen(9000,()=>{
     console.log(`Running server on http://localhost:9000`);
 })
