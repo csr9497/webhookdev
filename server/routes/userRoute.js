8ba43dfd0f58ac6ea043b312370de583648a0558
Cesar Oliva <coliva@inticousa.com>
2020-01-08 18:48:24 -0500

    adding code

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -20,19 +20,18 @@ const opti = (req, payload) =>{
     const rp = require('request-promise')
     headers = {
         'x-api-key': 'af6fdaaa-b9a5-4d26-9844-5c0a6b4cb2db', 
-        'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMTRkOWI3M2YyYWRiNGFhZjJlZjE4NiIsImlhdCI6MTU3ODQyNDc1OSwiZXhwIjoxNTc4NTExMTU5fQ.iZ4iBErkXKCWbJXp_JvqAaxP0OhETNcaGEvYWJ638lI'}
-    const {body } = req
-    //console.log(body)
-    const dir = `http://192.168.100.16:8000/service/5e14d9b73f2adb4aaf2ef186`
+        'x-access-token':
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMTVmMzRkMzRkMmZhMzE0YWM5MDYxNyIsImlhdCI6MTU3ODQ5Njg0NX0.1dEMiBAqeMdFsRhB86hUCCrNIogsou6Ulvij3NzgDwY'}
+    const dir = req.body.data.webhook
     const consult = async () => {
-    var options = {
-        method:'POST',
-        url: dir,
-        headers: headers,
-        json: true,
-        form: payload
-        }
-    return result = await rp(options)
+        var options = {
+            method:'POST',
+            url: dir,
+            headers: headers,
+            json: true,
+            form: payload
+            }
+        return result = await rp(options)
     }
     return consult
 }
